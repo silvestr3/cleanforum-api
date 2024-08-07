868ee46d017254325c66d379d94f91c86b6fbfea
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-07 16:29:30 -0300

    Create CurrentUserDecorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "src/auth/current-user.decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller("/questions")
@@ -8,7 +10,8 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
     return "create question";
   }
 }
