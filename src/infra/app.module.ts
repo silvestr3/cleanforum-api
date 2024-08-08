512e0d651846d58cf5bbfbb50c8847195fec285d
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-08 14:13:42 -0300

    Separate http layer into its own module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -3,10 +3,7 @@ import { PrismaService } from "@/infra/prisma/prisma.service";
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "@/infra/env";
 import { AuthModule } from "@/infra/auth/auth.module";
-import { CreateAccountController } from "@/infra/http/controllers/create-account.controller";
-import { AuthenticateController } from "@/infra/http/controllers/authenticate.controller";
-import { CreateQuestionController } from "@/infra/http/controllers/create-question.controller";
-import { FetchRecentQuestionsController } from "@/infra/http/controllers/fetch-recent-questions.controller";
+import { HttpModule } from "./http/http.module";
 
 @Module({
   imports: [
@@ -15,13 +12,7 @@ import { FetchRecentQuestionsController } from "@/infra/http/controllers/fetch-r
       isGlobal: true,
     }),
     AuthModule,
+    HttpModule,
   ],
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
-  providers: [PrismaService],
 })
 export class AppModule {}
