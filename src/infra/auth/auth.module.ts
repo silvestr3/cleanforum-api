d632fb37ac45b31486a44ed2bf4c7155b4e8f668
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-08 19:26:22 -0300

    define env module

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,22 @@
 import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
-import { Env } from "@/infra/env";
 import { JwtStrategy } from "./jwt.strategy";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./jwt-auth.guard";
+import { EnvService } from "../env/env.service";
+import { EnvModule } from "../env/env.module";
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
+      imports: [EnvModule],
       global: true,
-      inject: [ConfigService],
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
-        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+      inject: [EnvService],
+      useFactory(env: EnvService) {
+        const privateKey = env.get("JWT_PRIVATE_KEY");
+        const publicKey = env.get("JWT_PUBLIC_KEY");
 
         return {
           signOptions: { algorithm: "RS256" },
@@ -31,6 +32,7 @@ import { JwtAuthGuard } from "./jwt-auth.guard";
       useClass: JwtAuthGuard,
     },
     JwtStrategy,
+    EnvService,
   ],
 })
 export class AuthModule {}
