6818d136f6751d1e09cab89cabccf798b9668dc5
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-08 14:35:48 -0300

    created prisma repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
+import { PrismaQuestionsRepository } from "./repositories/prisma-questions-repository";
+import { PrismaQuestionCommentsRepository } from "./repositories/prisma-question-comments-repository";
+import { PrismaQuestionAttachmentsRepository } from "./repositories/prisma-question-attachments-repository";
+import { PrismaAnswersRepository } from "./repositories/prisma-answers-repository";
+import { PrismaAnswerCommentsRepository } from "./repositories/prisma-answer-comments-repository";
+import { PrismaAnswerAttachmentsRepository } from "./repositories/prisma-answer-attachments-repository";
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
 })
 export class DatabaseModule {}
