6c1e37e18a472ef331434bc70983b00b24aa6afc
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-09 15:11:56 -0300

    Implement delete question comment controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,20 +1,20 @@
 import { Module } from "@nestjs/common";
-import { CreateAccountController } from "./controllers/create-account.controller";
-import { AuthenticateController } from "./controllers/authenticate.controller";
-import { CreateQuestionController } from "./controllers/create-question.controller";
-import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 import { DatabaseModule } from "../database/database.module";
-import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
-import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
-import { CryptographyModule } from "../cryptography/cryptography.module";
-import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
-import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
 import { GetQuestionBySlug } from "./controllers/get-question-by-slug.controller";
-import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
-import { EditQuestionController } from "./controllers/edit-question.controller";
+import { CryptographyModule } from "../cryptography/cryptography.module";
 import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
-import { DeleteQuestionController } from "./controllers/delete-question.controller";
 import { DeleteQuestionUseCase } from "@/domain/forum/application/use-cases/delete-question";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
+import { EditQuestionController } from "./controllers/edit-question.controller";
+import { AuthenticateController } from "./controllers/authenticate.controller";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { CreateQuestionController } from "./controllers/create-question.controller";
+import { DeleteQuestionController } from "./controllers/delete-question.controller";
+import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { CommentOnQuestionController } from "./controllers/comment-on-question.controller";
 import { AnswerQuestionController } from "./controllers/answer-question.controller";
 import { AnswerQuestionUseCase } from "@/domain/forum/application/use-cases/answer-question";
 import { EditAnswerController } from "./controllers/edit-answer.controller";
@@ -22,11 +22,13 @@ import { EditAnswerUseCase } from "@/domain/forum/application/use-cases/edit-ans
 import { DeleteAnswerController } from "./controllers/delete-answer.controller";
 import { DeleteAnswerUseCase } from "@/domain/forum/application/use-cases/delete-answer";
 import { FetchQuestionAnswersController } from "./controllers/fetch-question-answers.controller";
+import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 import { FetchQuestionAnswersUseCase } from "@/domain/forum/application/use-cases/fetch-question-answers";
-import { ChooseQuestionBestAnswerController } from "./controllers/choose-question-best-answer.controller";
 import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-cases/choose-question-best-answer";
-import { CommentOnQuestionController } from "./controllers/comment-on-question.controller";
 import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/comment-on-question";
+import { DeleteQuestionCommentUseCase } from "@/domain/forum/application/use-cases/delete-question-comment";
+import { ChooseQuestionBestAnswerController } from "./controllers/choose-question-best-answer.controller";
+import { DeleteQuestionCommentController } from "./controllers/delete-question-comment.controller";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -44,6 +46,7 @@ import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/c
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
     CommentOnQuestionController,
+    DeleteQuestionCommentController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -59,6 +62,7 @@ import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/c
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
     CommentOnQuestionUseCase,
+    DeleteQuestionCommentUseCase,
   ],
 })
 export class HttpModule {}
