8db76d306fdb92c6f7b9c2dc0011c56d42d393a5
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-14 12:04:13 -0300

    Implement read notification controller

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,14 +1,17 @@
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import {
   Notification,
   NotificationProps,
-} from '@/domain/notification/enterprise/entities/notification'
+} from "@/domain/notification/enterprise/entities/notification";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaNotificationMapper } from "@/infra/database/prisma/mappers/prisma-notification-mapper";
 
 export function makeNotification(
   override: Partial<NotificationProps> = {},
-  id?: UniqueEntityID,
+  id?: UniqueEntityID
 ) {
   const notification = Notification.create(
     {
@@ -17,8 +20,25 @@ export function makeNotification(
       content: faker.lorem.sentence(10),
       ...override,
     },
-    id,
-  )
+    id
+  );
 
-  return notification
+  return notification;
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {}
+  ): Promise<Notification> {
+    const notification = makeNotification(data);
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    });
+
+    return notification;
+  }
 }
