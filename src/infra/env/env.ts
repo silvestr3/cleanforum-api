39d423856031233fb9fac86e1dc8cf5bf7b6eb09
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-14 15:14:13 -0300

    add cache module

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
@@ -8,7 +8,10 @@ export const envSchema = z.object({
   AWS_BUCKET_NAME: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
+  REDIS_HOST: z.string().optional().default("127.0.0.1"),
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_DB: z.coerce.number().optional().default(0),
   PORT: z.coerce.number().optional().default(3333),
-})
+});
 
-export type Env = z.infer<typeof envSchema>
+export type Env = z.infer<typeof envSchema>;
