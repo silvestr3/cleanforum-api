affaaa7ec08dcf3dae04afa745e4ec01e5869c19
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-20 09:53:32 -0300

    Return user name with jwt token

--- src/domain/forum/application/use-cases/authenticate-student.ts
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -1,55 +1,56 @@
-import { Either, left, right } from '@/core/either'
-import { Injectable } from '@nestjs/common'
-import { StudentsRepository } from '../repositories/students-repository'
-import { HashComparer } from '../cryptography/hash-comparer'
-import { Encrypter } from '../cryptography/encrypter'
-import { WrongCredentialsError } from './errors/wrong-credentials-error'
+import { Either, left, right } from "@/core/either";
+import { Injectable } from "@nestjs/common";
+import { StudentsRepository } from "../repositories/students-repository";
+import { HashComparer } from "../cryptography/hash-comparer";
+import { Encrypter } from "../cryptography/encrypter";
+import { WrongCredentialsError } from "./errors/wrong-credentials-error";
 
 interface AuthenticateStudentUseCaseRequest {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 type AuthenticateStudentUseCaseResponse = Either<
   WrongCredentialsError,
   {
-    accessToken: string
+    accessToken: string;
   }
->
+>;
 
 @Injectable()
 export class AuthenticateStudentUseCase {
   constructor(
     private studentsRepository: StudentsRepository,
     private hashComparer: HashComparer,
-    private encrypter: Encrypter,
+    private encrypter: Encrypter
   ) {}
 
   async execute({
     email,
     password,
   }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
-    const student = await this.studentsRepository.findByEmail(email)
+    const student = await this.studentsRepository.findByEmail(email);
 
     if (!student) {
-      return left(new WrongCredentialsError())
+      return left(new WrongCredentialsError());
     }
 
     const isPasswordValid = await this.hashComparer.compare(
       password,
-      student.password,
-    )
+      student.password
+    );
 
     if (!isPasswordValid) {
-      return left(new WrongCredentialsError())
+      return left(new WrongCredentialsError());
     }
 
     const accessToken = await this.encrypter.encrypt({
       sub: student.id.toString(),
-    })
+      name: student.name,
+    });
 
     return right({
       accessToken,
-    })
+    });
   }
 }
